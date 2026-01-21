de1e073771723fb511f882baf0b030385ac554a8
Denis Mathan <denis.mathan@gmail.com>
2026-01-21 23:32:14 +0100

    fixed / in dashboardcontroller

--- src/main/java/tutothr/common/controller/HomeController.java
+++ src/main/java/tutothr/common/controller/HomeController.java
@@ -39,7 +39,7 @@ public class HomeController {
 			model.addAttribute("pendingReportsCount", 0);
 		}
 
-		return "/views/dashboard";
+		return "views/dashboard";
 	}
 
 	private User getCurrentUserById() {
